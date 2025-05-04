a39e7f462149bf67a6d079b348a138581d8e4a10
cyspeo <cyspeo.gla@free.fr>
2025-05-04 20:37:06 +0200

    angular 18.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {ActivatedRoute, RouterOutlet} from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DropDownComponent],
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {}
